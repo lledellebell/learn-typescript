76fd64bb660903b06cf14459b5932644e7d25813
B <function.hola.b@gmail.com>
2025-01-16 15:20:47 +0900

    test: pull request action

--- algorithms/two-sum/dd_._._bb.ts
+++ algorithms/two-sum/dd_._._bb.ts
@@ -1,17 +1,19 @@
 /**
- * 
- * @param nums 
- * @param target 
- * @returns 
+ * 시간 복잡도: O(n)
+ * 공간 복잡도: O(n)
  */
-function twoSum(nums, target) {
-    const numMap = new Map();
+function twoSum(nums: number[], target: number): number[] {
+    const numMap = new Map<number, number>();
+
     for (let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
+
         if (numMap.has(complement)) {
-            return [numMap.get(complement), i];
+            return [numMap.get(complement)!, i];
         }
+
         numMap.set(nums[i], i);
     }
+
     return [];
 }
